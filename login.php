50cb491c172e2f70c446bfc8732ff666bbf8fa85
Hot_Boy_Xam_Tro <101193855+vinh333@users.noreply.github.com>
2022-12-08 09:25:47 +0700

    CHUC NANG LOGIN
    CHUC NANG LOGIN

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$email = $_POST['email'] ?? '';
+$password = $_POST['password'] ?? '';
+
+//ket noi csdl
+$db = mysqli_connect("localhost", "root", "", "csdl1");
+//truy van du lieu
+$sql = "select * from users where email='$email' and password='$password' ";
+//thuc hien truy van
+
+$rs = mysqli_query($db, $sql);
+
+if (mysqli_num_rows($rs) > 0) {
+    echo "dang nhap thanh cong";
+} else {
+    echo "that bai";
+    require_once 'login.html';
+}
