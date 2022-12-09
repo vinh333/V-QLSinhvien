d986c85711d2ad2902188b0417064760a35aac84
4nh <tranhanhle.2003@gmail.com>
2022-12-09 19:06:58 +0700

    chinh_sua

--- Registration.php
+++ Registration.php
@@ -1,83 +1,31 @@
 <?php
-
-
-// // $img = $_POST['img'] ?? '';
-// $username = $_POST['username'] ?? '';
-// $email = $_POST['email'] ?? '';
-// $password = $_POST['password'] ?? '';
-
-
-
-
-// if (isset($_POST['submit'])) {
-//    echo "dw3iuewi";
-//    //ket noi csdl
-//    $ketnoi = mysqli_connect("localhost", "root", "", "csdl1");
-//    //truy van du lieu
-
-//    $sql = "INSERT INTO `users` ( `username`, `email`, `password`) VALUES ( '$username', '$email', '$password'); ";
-//    //thuc hien truy van
-
-//    $ketqua = mysqli_query($ketnoi, $sql);
-
-
-//    // Kết nối cơ sở dữ liệu
-//    $conn = mysqli_connect('localhost', 'root', '', 'csdl1') or die('Lỗi kết nối');
-//    mysqli_set_charset($conn, "utf8");
-//    // Kiểm tra username hoặc email có bị trùng hay không
-//    $sql = "SELECT * FROM users WHERE username = '$username' OR email = '$email'";
-
-//    // Thực thi câu truy vấn
-//    $result = mysqli_query($ketnoi, $sql);
-
-//    // Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
-//    if (mysqli_num_rows($result) > 0) {
-//       echo '<script language="javascript">alert("Bị trùng tên hoặc chưa nhập tên!"); window.location="register.php";</script>';
-
-//       // Dừng chương trình
-//       die();
-//    } else {
-//       $sql = "INSERT INTO users (username, password, email) VALUES ('$username','$password','$email')";
-//       echo '<script language="javascript">alert("Đăng ký thành công!"); window.location="register.php";</script>';
-//    }
-// }
-
-
-header('Content-Type: text/html; charset=utf-8');
+   header('Content-Type: text/html; charset=utf-8');
 // Kết nối cơ sở dữ liệu
-$conn = mysqli_connect('localhost', 'root', '', 'csdl1') or die('Lỗi kết nối');
-mysqli_set_charset($conn, "utf8");
-
+   $conn = mysqli_connect('localhost', 'root', '', 'csdl1') or die('Lỗi kết nối');
+   mysqli_set_charset($conn, "utf8");
 // Dùng isset để kiểm tra Form
-if (isset($_POST['submit'])) {
-   $username = trim($_POST['username']);
-   $password = trim($_POST['password']);
-   $email = trim($_POST['email']);
+   if (isset($_POST['submit'])) {
+      $username = trim($_POST['username']);
+      $password = trim($_POST['password']);
+      $email = trim($_POST['email']);
+         if (empty($username)) {
+            array_push($errors, "Username is required");
+         }
+         if (empty($email)) {
+            array_push($errors, "Email is required");
+         }
 
-
-
-   if (empty($username)) {
-      array_push($errors, "Username is required");
-   }
-   if (empty($email)) {
-      array_push($errors, "Email is required");
-   }
-
-   if (empty($password)) {
-      array_push($errors, "Two password do not match");
-   }
-
-   // Kiểm tra username hoặc email có bị trùng hay không
+         if (empty($password)) {
+            array_push($errors, "Two password do not match");
+         }
+// Kiểm tra username hoặc email có bị trùng hay không
    $sql = "SELECT * FROM users WHERE username = '$username' OR email = '$email'";
-
-   // Thực thi câu truy vấn
+// Thực thi câu truy vấn
    $result = mysqli_query($conn, $sql);
-
-   // Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
+// Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
    if (mysqli_num_rows($result) > 0) {
       echo '<script language="javascript">alert("Bị trùng tên hoặc chưa nhập tên!"); window.location="Registration.php";</script>';
-
-      // Dừng chương trình
+// Dừng chương trình
       die();
    } else {
       //ket noi
@@ -96,7 +44,6 @@ if (isset($_POST['submit'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- login.php
+++ login.php
@@ -1,20 +1,19 @@
 <?php
-
-
-$email = $_POST['email'] ?? '';
-$password = $_POST['password'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
 
 //ket noi csdl
-$db = mysqli_connect("localhost", "root", "", "csdl1");
+    $db = mysqli_connect("localhost", "root", "", "csdl1");
 //truy van du lieu
-$sql = "select * from users where email='$email' and password='$password' ";
+    $sql = "select * from users where email='$email' and password='$password' ";
 //thuc hien truy van
 
-$rs = mysqli_query($db, $sql);
-mysqli_fetch_all($rs);
-if (mysqli_num_rows($rs) > 0) {
-    echo "dang nhap thanh cong";
-} else {
-    echo "that bai";
-    require_once 'login.html';
-}
+    $rs = mysqli_query($db, $sql);
+    mysqli_fetch_all($rs);
+    if (mysqli_num_rows($rs) > 0) {
+        echo "dang nhap thanh cong";
+    } else {
+        echo "that bai";
+        require_once 'login.html';
+    }
+?>
