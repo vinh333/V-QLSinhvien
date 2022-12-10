9905cc6651f1ae21ccdfaf75b1bd3e43da8effe8
4nh <tranhanhle.2003@gmail.com>
2022-12-10 14:42:29 +0700

    .

--- quanlygau/function.php
+++ quanlygau/function.php
@@ -1,27 +1,27 @@
-<!-- <?php
-    // function inSP($anh, $tensp, $gia, $kthuoc){
-    // echo "<div class=\"khungtrong\">";
-    //     echo "<img src=\"" .$anh. "\" >";
-    //     echo "<h2>" . $tensp . "<\h2>";
-    //     echo "<div class=\"thongtin\">";
-    //         echo "<p>" . $gia . "<\p>";
-    //         echo "<div class=\"thongso\">";
-    //             echo "<p>" . $kthuoc . "<\p>";
-    //             echo "<p>" . $kthuoc . "<\p>";
-    //         echo "<\div>";
-    //     echo "<\div>";
-    // echo "<\div>";
-    // }
-?> -->
+<?php
+    function inSP($anh, $tensp, $gia, $kthuoc){
+    echo "<div class=\"khungtrong\">";
+        echo "<img src=\"" .$anh. "\" >";
+        echo "<h2>" . $tensp . "</h2>";
+        echo "<div class=\"thongtin\">";
+            echo "<a>" . $gia . "</a>";
+            echo "<div class=\"thongso\">";
+                echo "<p>" . $kthuoc . "</p>";
+                echo "<p>" . $kthuoc . "</p>";
+            echo "</div>";
+        echo "</div>";
+    echo "</div>";
+    }
+?>
 
 <?php
     //session_start();
-     function inSP($anh, $tensp, $gia, $kthuoc){
-        echo "<div class=\"khungtrong\">";
-            echo "<img src=\"" .$anh. "\" >";
-            echo "<h2>" . $tensp . "</h2>";
-            echo "<h4>" . $gia . "</h4>";
-            echo "<p>" . $kthuoc .  $kthuoc. "</p>";
-        echo "</div>";
-    }
-?>
+    //  function inSP($anh, $tensp, $gia, $kthuoc){
+    //     echo "<div class=\"khungtrong\">";
+    //         echo "<img src=\"" .$anh. "\" >";
+    //         echo "<p class=\"thongtin\">" . $tensp . "</p>";
+    //         echo "<h4>" . $gia . "</h4>";
+    //         echo "<div class=\"thongso\">" . $kthuoc . " x " . $kthuoc. "</div>";
+    //     echo "</div>";
+    //}
+?> 
